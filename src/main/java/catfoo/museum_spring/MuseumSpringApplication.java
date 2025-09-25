7273d9509549a5b111fade8ce3991b46fdf496c7
So Youn Choi <manghatuhyo@gmail.com>
2025-09-25 09:43:06 +0200

    Initial commit

---
+++ src/main/java/catfoo/museum_spring/MuseumSpringApplication.java
@@ -0,0 +1,13 @@
+package catfoo.museum_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseumSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/catfoo/museum_spring/MuseumSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package catfoo.museum_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
